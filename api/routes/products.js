43112de03f2d665dd3a3159c40a38a3505519d81
ryanzola <ryanzola@me.com>
2018-01-11 01:11:11 -0500

    access control allow methods, handling CORS

---
+++ api/routes/products.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "handling GET requests to /products"
+  });
+});
+
+router.post("/", (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  }
+
+  res.status(201).json({
+    message: "handling POST requests to /products",
+    createdProduct: product
+  });
+});
+
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  if (id === "special") {
+    res.status(200).json({
+      message: "you discovered the special id"
+    });
+  } else {
+    res.status(200).json({
+      message: "you passed an id"
+    });
+  }
+});
+
+router.patch("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "updated product"
+  });
+});
+
+router.delete("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "product deleted"
+  });
+});
+
+module.exports = router;
