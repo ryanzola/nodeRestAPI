9f49f391fd252206ea937d8c13ff90f5e1236983
ryanzola <ryanzola@me.com>
2018-01-12 15:56:42 -0500

    more expressive status messages

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,47 +1,132 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const multer = require("multer");
+const upload = multer({destination: "uploads/"})
+
+const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "handling GET requests to /products"
-  });
+  Product.find()
+    .select("name price _id")
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            url: {
+              request: {
+                type: "GET",
+                url: "http://localhost:3000/products/" + doc._id
+              }
+            }
+          };
+        })
+      };
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post("/", (req, res, next) => {
-  const product = {
+router.post("/", upload.single('productImage'), (req, res, next) => {
+  console.log(req.file);
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
-  }
-
-  res.status(201).json({
-    message: "handling POST requests to /products",
-    createdProduct: product
   });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "created product successfully",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products/" + result._id
+          }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  if (id === "special") {
-    res.status(200).json({
-      message: "you discovered the special id"
+  Product.findById(id)
+    .select("name price _id")
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: "no valid entry found for provided ID"
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
-  } else {
-    res.status(200).json({
-      message: "you passed an id"
-    });
-  }
 });
 
 router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "updated product"
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: 'product successfully updated',
+        request: {
+          type: "GET",
+          url: "http//localhost:3000/products" + id
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "product deleted"
-  });
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "product successfully deleted"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect("mongodb://ryanzola:" + process.env.MONGO_ATLAS_PW + "@node-rest-shop-shard-00-00-hce2j.mongodb.net:27017,node-rest-shop-shard-00-01-hce2j.mongodb.net:27017,node-rest-shop-shard-00-02-hce2j.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin", {useMongoClient: true})
+mongoose.Promise = global.Promise;
 
 // middleware
 app.use(morgan("dev"));
