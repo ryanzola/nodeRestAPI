55a4989f83b33ad2fce9e74929fe14267fd3afa7
ryanzola <ryanzola@me.com>
2018-01-18 01:42:06 -0500

    jwt authentication and protected routes added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user");
 
 mongoose.connect("mongodb://ryanzola:" + process.env.MONGO_ATLAS_PW + "@node-rest-shop-shard-00-00-hce2j.mongodb.net:27017,node-rest-shop-shard-00-01-hce2j.mongodb.net:27017,node-rest-shop-shard-00-02-hce2j.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin", {useMongoClient: true})
 mongoose.Promise = global.Promise;
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
