9361f6b3a120d679ce004c62ed3451f13aa2be7d
ryanzola <ryanzola@me.com>
2018-01-20 12:46:43 -0500

    Main functionality complete TODO: add profile picture to user model

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const checkAuth = require("../middleware/check-auth")
+const checkAuth = require("../middleware/check-auth");
 
-const ProductsController = require("../controllers/products")
+const ProductsController = require("../controllers/products");
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
@@ -15,14 +15,14 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if( file.mimetype === "image/jpeg" || file.mimetype === "image/png" ) {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
     cb(null, true);
   } else {
     cb(null, false);
   }
-}
-const upload = multer({ 
-  storage: storage, 
+};
+const upload = multer({
+  storage: storage,
   limits: {
     fileSize: 1024 * 1024 * 5
   },
--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Accept, Authorization"
+    "Origin, X-Requested-With, Accept, Authorization, Content-Type"
   );
 
   if (req.method === "OPTIONS") {
