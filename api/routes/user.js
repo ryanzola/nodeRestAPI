5d12ac905a52262db4571f13e9c038776cd46b63
ryanzola <ryanzola@me.com>
2018-04-10 15:35:17 -0400

    more checkAuth

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-//import checkAuth from '../middleware/check-auth';
+import checkAuth from '../middleware/check-auth';
 import UserController from '../controllers/user';
 
 const router = Router();
 
 router.post('/signup', UserController.user_signup);
 router.post('/login', UserController.user_login);
-router.delete('/:userId',  UserController.user_delete);
+router.delete('/:userId', checkAuth, UserController.user_delete);
 
 export default router;
