e6f0b01eaa0539c26a1b232eccc6f42380da0ec5
ryanzola <ryanzola@me.com>
2018-04-10 15:34:47 -0400

    es6 syntax updates

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,4 +9,4 @@ router.post('/', checkAuth, OrdersController.orders_create_order);
 router.get('/:orderId', checkAuth, OrdersController.orders_get_order);
 router.delete('/:orderId', checkAuth, OrdersController.orders_delete_order);
 
-module.exports = router;
+export default router;
